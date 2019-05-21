c9a88d6a078413e536662170f10fc22ca18ab7ae
Jew <kittichok.dev@gmail.com>
2019-05-21 13:25:52 +0700

    Updates

---
+++ precache-manifest.15287ffe30f825a563c6ce1e77d436ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9866292b1314f0c95417f91fca782a5",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "cf844998b41086e331c2",
+    "url": "/website/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "984d0629a60e5e46fc49",
+    "url": "/website/static/css/main.1c37afbf.chunk.css"
+  },
+  {
+    "revision": "cf844998b41086e331c2",
+    "url": "/website/static/js/2.84e9e19a.chunk.js"
+  },
+  {
+    "revision": "984d0629a60e5e46fc49",
+    "url": "/website/static/js/main.91038e23.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "ba546692890a073964edeb962517d8c5",
+    "url": "/website/static/media/data.ba546692.csv"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/website/static/media/logo.ee7cd8ed.svg"
+  }
+]);
