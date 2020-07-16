a7f94484c221c9ac656a363a71823b78fc3b977a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-16 11:01:28 +0000

    Deploy Kittichok/website to github.com/Kittichok/website.git:gh-pages

---
+++ precache-manifest.4b773487383596c1cae4211537dda7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45793d9a32d8e987722181642f860cd",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "ae50df46c5a32be4dec2",
+    "url": "/website/static/css/2.2ff68a77.chunk.css"
+  },
+  {
+    "revision": "5b31852d63f37279a331",
+    "url": "/website/static/css/main.603eaff0.chunk.css"
+  },
+  {
+    "revision": "ae50df46c5a32be4dec2",
+    "url": "/website/static/js/2.d8d7f1fd.chunk.js"
+  },
+  {
+    "revision": "59eb19a73f94b694d760132866c0a6a0",
+    "url": "/website/static/js/2.d8d7f1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b31852d63f37279a331",
+    "url": "/website/static/js/main.d88356ae.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  }
+]);
